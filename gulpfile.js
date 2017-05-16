a8c6faecd0fd57926bb8640989810fa2ad582fbe
David Mogollon <dmogollon@uninorte.edu.co>
2017-05-16 17:31:12 -0500

    Added EsLint for error detection.
    the autogrow.js had a semicolom before an anonymous function, this
    generated an error and was detected by EsLint.

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,32 @@
  	  sass = require('gulp-sass'),
  	  maps = require('gulp-sourcemaps'),
  	  imagemin = require('gulp-imagemin'),
- 	  clean = require('gulp-clean');
+ 	  clean = require('gulp-clean'),
  	  //extra credits
-
+ 	  eslint = require('gulp-eslint'),
  	  //for livereload you must install a chrome extension : 
  	 // https://chrome.google.com/webstore/search/livereload?hl=es-419
  	  livereload = require('gulp-livereload')
 
 // Scripts Task
 
-//concat scripts, do it on dev server
-gulp.task("concatScripts", function() {
-	return gulp.src('js/**/*.js')
+//it will clear the global.js and global.js.map files every time the script task runs.
+gulp.task('clean-scripts', function () {
+  return gulp.src(['js/global.js','js/global.js.map'])
+    .pipe(clean());
+});
+//concat scripts on global.js files and write a sourcemap
+gulp.task("concatScripts",['clean-scripts'], function() {
+	return gulp.src(['js/**/*.js','!global.js'])
     .pipe(maps.init())
     .pipe(concat('global.js'))
     .pipe(maps.write('./'))
     .pipe(gulp.dest('js'));
 });
 
-// should be minify scripts
-gulp.task('scripts',['concatScripts'], function(){
-	gulp.src('js/**/*.js')
+// Minify the script, rename it and copy it on the dist js folder
+gulp.task('scripts',['lint','concatScripts'], function(){
+	gulp.src('js/global.js')
 	.pipe(uglify())
 	.pipe(rename('all.min.js'))
 	.pipe(gulp.dest('dist/js'));
@@ -35,6 +40,8 @@ gulp.task('scripts',['concatScripts'], function(){
 
 
 // Styles Task
+
+//compile to css
 gulp.task('compileSass', function(){
 	return gulp.src('sass/**/*.scss')
 	// .pipe(plumber())
@@ -47,6 +54,7 @@ gulp.task('compileSass', function(){
 	.pipe(gulp.dest('css'));
 
 });
+
 gulp.task('styles',['compileSass'], function(){
 	return gulp.src('css/**/*.css')
 	 .pipe(concat('all.min.css'))//concatenate
@@ -72,6 +80,24 @@ gulp.task('clean',function(){
 	return gulp.src('dist/*')
 	.pipe(clean());
 });
+
+gulp.task('lint', function(){
+    // ESLint ignores files with "node_modules" paths. 
+    // So, it's best to have gulp ignore the directory as well. 
+    // Also, Be sure to return the stream from the task; 
+    // Otherwise, the task may end before the stream has finished. 
+    return gulp.src(['js/**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property 
+        // of the file object so it can be used by other modules. 
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console. 
+        // Alternatively use eslint.formatEach() (see Docs). 
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on 
+        // lint error, return the stream and pipe to failAfterError last. 
+        .pipe(eslint.failAfterError());
+});
+
 //Watch Task
 //Watch JS
 
